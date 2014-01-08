312f519785574486750c31316446beffe08c3f95
Michael JasonSmith <mpj17@onlinegroups.net>
2014-01-08 13:40:28 +1300

    Export some useful things.

--- gs/group/member/add/base/__init__.py
+++ gs/group/member/add/base/__init__.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
-# This space intentially left blank
+from __future__ import absolute_import
+#lint:disable
+from .adder import Adder
+from .addfields import AddFields
+from .audit import ADD_NEW_USER, ADD_OLD_USER, ADD_EXISTING_MEMBER
+#lint:enable
