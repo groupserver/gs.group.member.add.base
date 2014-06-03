f2ecd21c3375a985a7840fbf58f0923709403600
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-03 10:50:51 -0700

    Making a release

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-version = '2.0.0'
-release = False
+version = '2.1.0'
+release = True
 
 #-----------------------------------------------------------------------------#
 import sys
@@ -63,8 +63,9 @@ def get_version():
         else:
             version_string = "%s.dev%s-%s" % (version, datestring, globalid)
 
-    except (CommandError, ValueError, TypeError) as e:
-        print (e)
+    except (CommandError, ValueError, TypeError):
+        # --=mpj17=-- Usually because we are building out a source-egg, rather
+        # than from a Hg source-directory.
         version_string = parse_version_from_package()
 
     return version_string
