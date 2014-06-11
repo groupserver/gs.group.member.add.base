2e160dfedc3ce459fd4fad15e8e160491716e079
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-11 12:46:52 -0700

    Updating the product metadata.

--- setup.py
+++ setup.py
@@ -12,17 +12,22 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='gs.group.member.add.base',
     version=version,
     description="Add people to a GroupServer group, without an invitation",
-    long_description=open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -39,7 +44,7 @@ setup(name='gs.group.member.add.base',
     keywords='group, membership, add, group administration',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='http://groupserver.org',
+    url='https://source.iopen.net/groupserver/gs.group.member.add.base/',
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.group', 'gs.group.member',
