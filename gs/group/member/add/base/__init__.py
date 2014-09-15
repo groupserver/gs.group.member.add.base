f32538a555d57ab0354124c2dbe58acac33811f3
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-15 10:27:19 -0700

    Adding the MessageFactory

--- gs/group/member/add/base/__init__.py
+++ gs/group/member/add/base/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 #lint:disable
+from zope.i18nmessageid import MessageFactory
+GSMessageFactory = MessageFactory('gs.group.member.add')
 from .adder import Adder
 from .addfields import AddFields
 from .audit import ADD_NEW_USER, ADD_OLD_USER, ADD_EXISTING_MEMBER
